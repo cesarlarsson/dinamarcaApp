b1773f4206b8334f752f9553865dc7095bd0dc43
GLOBANT\cesar.suarez <cesar.suarez@globant.com>
2019-12-19 13:22:14 -0500

    qrscan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-//import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Toast } from '@ionic-native/toast/ngx';
